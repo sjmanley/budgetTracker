a0a75aa7c88770e0dbf3c08633eaa647b9d222e9
Scott Manley <scottmanley@SJM-MacBook-Pro.local>
2020-04-18 15:16:42 -0500

    revising server.js file fro routes

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
+  
 });
